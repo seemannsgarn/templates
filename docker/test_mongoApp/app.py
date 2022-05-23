8e703cab75656c38c534732bd6452b1c731ef16e
musta700@gmail.com <musta700@gmail.com>
2022-05-23 16:52:50 +0300

    updated 23.05.2022

--- docker/test_mongoApp/app.py
+++ docker/test_mongoApp/app.py
@@ -1,13 +1,14 @@
+import time
 from uuid import uuid4
-from docker.test_mongoApp.storage import MongodbService
-from pymongo import MongoClient
+from storage import MongodbService
 
 storage = MongodbService.get_instance()
 
 for _ in range(5):
     dto = {
         "_id":str(uuid4()),
-        "payload":str(uuid4())
+        "payload":str(uuid4()),
+        # "field2":str(int(time.time()))
     }
     storage.save_data(dto)
 
